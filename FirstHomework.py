79cce96fc7b030d5e1f5bb7c33bed3875a8f49aa
Dasha <darja.portnova@gmail.com>
2023-12-04 23:00:50 +0200

    task 2 and 3 done!

--- FirstHomework.py
+++ FirstHomework.py
@@ -1,8 +1,30 @@
 import math
-#2.2 Radius of the Earth -6378km. Diameter of a 2 euro coin is 25.75 millimeters
-EARTH_RADIUS_KM = 6378
-TWO_EURO_COIN_MM = 25.75
+#4
 
+#3
+phrase_1 = "kill-koll"
+phrase_2 = "killadi-koll"
+phrase_3 = "killkoll"
+
+for i in range(1, 10):
+  if i == 3 or i == 6:
+    print(phrase_2, end=' ')
+  else:
+    print(phrase_1, end=' ')
+print(phrase_3)
+print(phrase_1)
+
+#2.2 Radius of the Earth -6378km. Diameter of a 2 euro coin is 25.75 millimeters
+earth_radius_km = 6378
+coin_diameter_mm = 25.75
+
+earth_radius_m = earth_radius_km * 1000
+coin_diameter_m = coin_diameter_mm / 1000
+
+circumference_of_earth_m = 2 * math.pi * earth_radius_m
+number_of_coins = circumference_of_earth_m / coin_diameter_m
+
+print(f"Number of coins: {number_of_coins:.0f}")
 
 #2.1 Ruudu sees asub ring. Ringi raadius on R. S,P-SQUARE; S,P-CIRCLE ???
 r = float(input("There is a circle inside a square, write the radius for it: "))
