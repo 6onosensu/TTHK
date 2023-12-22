bba1eff1f6f088454da9975a77a977935f94e490
Darya Suhhanova <darja.portnova@gmail.com>
2023-12-22 16:34:44 +0200

    the game is functioning now

--- lesson4/rock_paper_scissors.py
+++ lesson4/rock_paper_scissors.py
@@ -1,26 +1,33 @@
 import random as r
 
 choices = ["Rock", "Paper", "Scissors"]
-player = {1: [], 2: [], 3: []}
-player2 = {1: [], 2: [], 3: []}
+winning_combinations = {
+    ("Rock", "Scissors"): "Rock",
+    ("Scissors", "Paper"): "Scissors",
+    ("Paper", "Rock"): "Paper"
+}
 
-
+begin = "Do you want to play rock-paper-scissors? Yes/No: "
 while True:
-    txt = (
-        "Do you want to play rock-paper-scissors? "
-        "Yes/No: "
-    )
-    start = input(txt).capitalize().strip()
+    start = input(begin).capitalize().strip()
     if start[0] == "Y":
         try:
             txt = "Enter a number of players (1 to 2): "
             num_players = int(input(txt))
             if 1 <= num_players <= 2:
                 txt = "Enter your names separated by a ',' and a space: "
-                names = input(txt).split(", ")
+                while True:
+                    names = input(txt).split(", ")
+                    condition1 = len(names) == num_players
+                    condition2 = (num_players == 1 and len(names) == 1)
+                    if condition1 or condition2:
+                        break
+                    else:
+                        print("Please enter the correct number of names.")
                 players_moves = {name: {} for name in names}
-                print(names, players_moves)
-                print("The game has begun!")
+                if num_players == 1:
+                    players_moves["Computer"] = {}
+                print("\nThe game has begun!")
                 break
             else:
                 print("Please enter a number 1 or 2.")
@@ -30,31 +37,58 @@ while True:
         print("Okey, maybe another time!")
         break
 
-
 if num_players == 1:
-    players_moves["Computer"] = {}
     for count in range(1, 4):
         # For player
         txt = "Enter your choice (Rock/Paper/Scissors): "
         player_choice = input(txt).capitalize()
-        players_moves[names[0]][count] = player_choice
+        if player_choice in choices:
+            players_moves[names[0]][count] = player_choice
         # player2 = computer
         r.shuffle(choices)
         computer_choice = choices[0]
         print(f"Computer chose {computer_choice}")
         players_moves["Computer"][count] = computer_choice
-
-
 else:
     for count in range(1, 4):
         for name in names:
             txt = (
-                "Player {0},"
+                "Player {0}, "
                 "enter your choice (Rock/Paper/Scissors): "
             ).format(name)
             choice = input(txt).capitalize()
             players_moves[name][count] = choice
 
-print("Final moves:")
+print("\nFinal moves:")
 for player, moves in players_moves.items():
-    print(f"{player}: {moves}")
+    print(f"Player {player} chose: {moves}")
+
+player1, player2 = players_moves.keys()
+count1 = 0
+count2 = 0
+
+for i in range(1, 4):
+    player1_choice = players_moves[player1][i]
+    player2_choice = players_moves[player2][i]
+    if player1_choice == player2_choice:
+        print(f"Round {i}: Tie")
+        count1 += 1
+        count2 += 1
+    elif winning_combinations.get((player1_choice, player2_choice)) == player1_choice:
+        print(f"Round {i}: {player1} wins")
+        count1 += 1
+    elif winning_combinations.get((player2_choice, player1_choice)) == player2_choice:
+        print(f"Round {i}: {player2} wins")
+        count2 += 1
+    else:
+        print(f"Round {i}: No clear winner")
+
+if count1 == count2:
+    winner = "Tie"
+elif count1 > count2:
+    winner = player1
+else:
+    winner = player2
+
+print(f"\nTotal wins: {player1} - {count1}, {player2} - {count2}")
+print(f"The winner is {winner}")
