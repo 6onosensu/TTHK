d7cf2574eaf1e1e31c939e1a607843d9ad5bd2cb
Dasha <darja.portnova@gmail.com>
2024-01-30 20:18:33 +0200

    added edit function

--- sonastik/functions.py
+++ sonastik/functions.py
@@ -1,3 +1,5 @@
+asked_to_add_word = False
+
 def read_file(file):
     file = open(file,'r',encoding="utf-8")
     list = [] 
@@ -6,20 +8,84 @@ def read_file(file):
     file.close()
     return list
 
-def is_latin_letter(letter):
-    return '\u0400' <= letter <= '\u04FF'
+def write_to_file(file_path, data_list):
+    with open(file_path, 'w', encoding='utf-8') as file:
+        for item in data_list:
+            file.write(item + '\n')
 
-def is_russian_letter(letter):
+
+def is_latin_letter(letter):
     return '\u0041' <= letter <= '\u005A' or '\u0061' <= letter <= '\u007A'
 
+def is_russian_letter(letter):
+    return '\u0400' <= letter <= '\u04FF'
+
+def show_words(eng: list, rus: list):
+    for e, r in zip(eng, rus):
+        print(e, ":", r)
+
+def question(eng: list, rus: list, word: str):
+    global asked_to_add_word
+    if asked_to_add_word:
+        return eng, rus
+
+    txt = f"Would you like to add word '{word}' to list?: "
+    i = input(txt).lower()
+    if i == "yes":
+        eng, rus = add_to_list(eng, rus, word)
+        asked_to_add_word = True
+    else:
+        print("OK, maybe later!")
+        asked_to_add_word = True
+    return eng, rus
+
+# box - коробка
+def add_to_list(eng: list, rus: list, word: str):
+    txt = f"Enter the translation of the {word}: "
+    if is_latin_letter(word[0]):
+        translation = input(txt).capitalize()
+        eng.append(word)
+        rus.append(translation)
+    elif is_russian_letter(word[0]):
+        translation = input(txt).capitalize()
+        eng.append(translation)
+        rus.append(word)
+    return eng, rus
+
 def translate_to_russian(eng: list, rus: list, word: str):
+    translated_word = "Translation not found"
+    word = word.lower()
     for i, search_w in enumerate(eng):
-        if search_w == word:
-            return rus[i]
+        if search_w.lower() == word:
+            translated_word = rus[i]
+            print(f"{search_w} - {translated_word}")
+            break
+    return translated_word
 
 def translate_to_english(rus: list, eng: list, word: str):
-    word = word.capitalize()
+    translated_word = "Translation not found"
+    word = word.lower()
     for search_w, eng_w in zip(rus, eng):
-        if search_w == word:
-            return eng_w
-        
+        if search_w.lower() == word:
+            translated_word = eng_w
+            print(f"{search_w} - {eng_w}")
+            break
+    return translated_word
+
+def edit(eng: list, rus: list):
+    txt = "Enter a word to edit it: "
+    enter = input(txt).lower()
+
+    for i, (rw, ew) in enumerate(zip(rus, eng)):
+        if rw.lower() == enter or ew.lower() == enter:
+            txt1 = "Enter the correct word in Russian: "
+            edited_word_rus = input(txt1)
+
+            txt2 = "Enter the correct word in English: "
+            edited_word_eng = input(txt2)
+
+            rus[i] = edited_word_rus.capitalize()
+            eng[i] = edited_word_eng.capitalize()
+            return eng, rus
+    print(f"The word '{enter}' not found!")
+    return eng, rus
--- sonastik/main.py
+++ sonastik/main.py
@@ -1,20 +1,46 @@
 from functions import *
 
-txt = "translation of a word: t 'word'"
+
+txt = """
+translation of a word: t 'word'
+to show all words: show
+to edit a word: edit
+
+    : """
 while True:
-    enter = input(txt).split()
-    if enter == "quit":
+    user_input = input(txt).lower().strip()
+    if not user_input:
+        continue
+
+    if user_input[0] == "quit":
         break
 
-    rus: list = read_file("rus.txt")
-    eng: list = read_file("eng.txt")
+    try:
+        rus: list = read_file("./sonastik/rus.txt")
+        eng: list = read_file("./sonastik/eng.txt")
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+        continue
 
-    if enter[0] == "t":
-        first_letter = enter[1].split("")[0]
+    if user_input.startswith("t "):
+        word_to_translate = user_input.split(maxsplit=1)[1]
+        first_letter = word_to_translate[0]
         if is_latin_letter(first_letter):
-            rus_w = translate_to_russian(eng, rus, enter[1])
+            translated_word = translate_to_russian(eng, rus, word_to_translate)
+            if translated_word == "Translation not found":
+                eng, rus = question(eng, rus, word_to_translate)
+                write_to_file("./sonastik/eng.txt", eng)
+                write_to_file("./sonastik/rus.txt", rus)
         elif is_russian_letter(first_letter):
-            eng_w = translate_to_english(rus, eng, enter[1])
-            
-
+            translated_word = translate_to_english(eng, rus, word_to_translate)
+            if translated_word == "Translation not found":
+                eng, rus = question(eng, rus, word_to_translate)
+                write_to_file("./sonastik/eng.txt", eng)
+                write_to_file("./sonastik/rus.txt", rus)
+    elif user_input.startswith("show"):
+        show_words(eng, rus)
+    elif user_input.startswith("edit"):
+        eng, rus = edit(eng, rus)
+        write_to_file("./sonastik/eng.txt", eng)
+        write_to_file("./sonastik/rus.txt", rus)
 
