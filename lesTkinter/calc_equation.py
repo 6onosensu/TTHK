b204dad1212458771a6b13faaa394fe9538af682
Dasha <darja.portnova@gmail.com>
2024-02-13 18:46:00 +0200

    new icon, added fields and buttons to window(root)

--- lesTkinter/calc_equation.py
+++ lesTkinter/calc_equation.py
@@ -1,29 +1,42 @@
 from tkinter import *
 
 
-calc_window = Tk()
-calc_window.geometry("600x200")
-calc_window.title("Quadratic equation")
-calc_window.iconbitmap("calculator.ico")
+root = Tk()
+root.geometry("600x300")
+root.title("Quadratic equation")
+root.iconbitmap("q.ico")
 
+lab = Label(root, text="Solving a quadratic equation", bg="blue",
+            fg="white", font="Arial 24", height=1,)
+lab.pack(fill=X)
 
+equ_frame = Frame(root)
+equ_frame.pack(pady=20)
 
-lab = Label(text="Solving a quadratic equation", bg="#1b6478", fg="white", font="Arial 24",
-            height=1,)
+a_entry = Entry(equ_frame, width=10)
+a_entry.grid(row=0, column=0)
+a_label = Label(equ_frame, text="x² + ")
+a_label.grid(row=0, column=1)
 
-row = []
-ent_a = Entry(row, bg="#1b6478", fg="white", font="Arial 18")
-ent_b = Entry(row, bg="#1b6478", fg="white", font="Arial 18")
-ent_c = Entry(row, bg="#1b6478", fg="white", font="Arial 18")
+b_entry = Entry(equ_frame, width=10)
+b_entry.grid(row=0, column=2)
+b_label = Label(equ_frame, text="x + ")
+b_label.grid(row=0, column=3)
 
+c_entry = Entry(equ_frame, width=10)
+c_entry.grid(row=0, column=4)
 
-objects = [lab, row]
+equals_label = Label(equ_frame, text=" = 0")
+equals_label.grid(row=0, column=5)
 
-for i in range(len(objects)):
-    objects[i].pack()
+solve_button = Button(equ_frame, text="Solve", bg="green", fg="white")
+solve_button.grid(row=0, column=6)
 
-row = [ent_a, ent_b, ent_c]
-for element in range(len(row)):
-    row[element].grid()
+solution_frame = Frame(root, bg="yellow", height=100, width=400)
+solution_frame.pack(fill=X, expand=True)
+
+solution_label = Label(solution_frame, text="Solution", bg="yellow")
+solution_label.pack(pady=40)
+
     
-calc_window.mainloop()
+root.mainloop()
